24e9d30d476a20d84e7c2ce5c444b44fb7afde9e
Linn2Htet <linnlinnhtet36@gmail.com>
2024-05-28 18:52:26 +0630

    change fixture image

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/first-fixtures', async(req, res, next) => {
         return {
             "title": `${fixture['5780']}  -  ${fixture['5782']}`,
             "subtitle": `${fixture['5769']} | ${fixture['5779']} (UTC +6:30)`,
-            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/a4d0922a1cca11efbd77163fadc66a8a.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/cde6c79c1cca11ef80f5ae529ffde60d.jpeg",
+            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/047c85d01cdf11efb6a2c665e4da7031.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/05c05b2e1cdf11ef822dc665e4da7031.jpeg",
             "url": '',
             "buttons": [
                 {
@@ -94,7 +94,7 @@ router.get('/second-fixtures', async(req, res, next) => {
         return {
             "title": `${fixture['5780']}  -  ${fixture['5782']}`,
             "subtitle": `${fixture['5769']} | ${fixture['5779']} (UTC +6:30)`,
-            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/a4d0922a1cca11efbd77163fadc66a8a.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/cde6c79c1cca11ef80f5ae529ffde60d.jpeg",
+            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/047c85d01cdf11efb6a2c665e4da7031.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/05c05b2e1cdf11ef822dc665e4da7031.jpeg",
             "url": '',
             "buttons": [
                 {
@@ -161,7 +161,7 @@ router.get('/third-fixtures', async(req, res, next) => {
         return {
             "title": `${fixture['5780']}  -  ${fixture['5782']}`,
             "subtitle": `${fixture['5769']} | ${fixture['5779']} (UTC +6:30)`,
-            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/a4d0922a1cca11efbd77163fadc66a8a.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/cde6c79c1cca11ef80f5ae529ffde60d.jpeg",
+            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/047c85d01cdf11efb6a2c665e4da7031.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/05c05b2e1cdf11ef822dc665e4da7031.jpeg",
             "url": '',
             "buttons": [
                 {
@@ -227,7 +227,7 @@ router.get('/fourth-fixtures', async(req, res, next) => {
         return {
             "title": `${fixture['5780']}  -  ${fixture['5782']}`,
             "subtitle": `${fixture['5769']} | ${fixture['5779']} (UTC +6:30)`,
-            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/a4d0922a1cca11efbd77163fadc66a8a.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/cde6c79c1cca11ef80f5ae529ffde60d.jpeg",
+            "image": index%2===0?"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/047c85d01cdf11efb6a2c665e4da7031.jpeg":"https://s3-ap-southeast-1.amazonaws.com/myalice-live-public-bucket/misc/05c05b2e1cdf11ef822dc665e4da7031.jpeg",
             "url": '',
             "buttons": [
                 {
