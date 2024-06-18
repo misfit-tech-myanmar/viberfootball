fe07c88d127d52fb52ac42dba5d839b4224b0b3e
Linn2Htet <linnlinnhtet36@gmail.com>
2024-06-18 08:45:59 +0630

    store fixtures to redis

--- services/store.redis.datalab.service.js
+++ services/store.redis.datalab.service.js
@@ -14,8 +14,8 @@ StoreRedisFromDatalab.prototype = {
             const userResponse = await self.Axios.get(`/stable/bots/labs/2241/entries`);
             const users = await self.Axios.get(`/stable/bots/labs/2241/entries?limit=${userResponse.data.count}`);
 
-            // const fixtureResponse = await self.Axios.get(`/stable/bots/labs/2247/entries`);
-            // const fixtures = await self.Axios.get(`/stable/bots/labs/2247/entries?limit=${fixtureResponse.data.count}`);
+            const fixtureResponse = await self.Axios.get(`/stable/bots/labs/2247/entries`);
+            const fixtures = await self.Axios.get(`/stable/bots/labs/2247/entries?limit=${fixtureResponse.data.count}`);
             
             // const predictionResponse = await self.Axios.get(`/stable/bots/labs/2268/entries`);
             // const predictions = await self.Axios.get(`/stable/bots/labs/2268/entries?limit=${predictionResponse.data.count}`);
@@ -30,13 +30,13 @@ StoreRedisFromDatalab.prototype = {
                     console.log('users set to redis:', reply);
                 }
             })
-            // await self.RedisClient.set("fixtures", JSON.stringify(fixtures.data.dataSource), function(err, reply) {
-            //     if (err) {
-            //         console.error('Error setting key:', err);
-            //     } else {
-            //         console.log('Fixtures set to redis :', reply);
-            //     }
-            // })
+            await self.RedisClient.set("fixtures", JSON.stringify(fixtures.data.dataSource), function(err, reply) {
+                if (err) {
+                    console.error('Error setting key:', err);
+                } else {
+                    console.log('Fixtures set to redis :', reply);
+                }
+            })
             // await self.RedisClient.set("user-predictions", JSON.stringify(predictions.data.dataSource), function(err, reply) {
             //     if (err) {
             //         console.error('Error setting key:', err);
