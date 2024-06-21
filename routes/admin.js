114c9700ca772a32a811591b61c4c2244682ebc5
Linn2Htet <linnlinnhtet36@gmail.com>
2024-06-21 13:48:56 +0630

    admin dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -6,19 +6,31 @@ const router = express.Router();
 
 const AuthController = require('../controllers/admin/auth.controller');
 const UserController = require('../controllers/admin/dashboard.controller');
+const LeaderBoardService = require('../services/leaderboard.service');
+const leaderBoardService = new LeaderBoardService();
 
 router.get('/login', redirectIfAuthenticated, async(req, res) => {
-    console.log("renderrrrr")
     res.render('login');
 })
 router.post('/login', (req, res) => AuthController.login(req, res))
 
-router.get('/dashboard', checkAuth,  (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', checkAuth,  async(req, res) => {
+    const leaderBoards = await leaderBoardService.getTopPredictionUserScore();
+    let ranksWithName = leaderBoards.inter.map((user, index)=> {
+        return {
+            name: user['5751']===''?'Unknown':user['5751'],
+            phone: user['5752'],
+            scores: user['5755']
+        }
+    })
+    res.render('dashboard', {
+        leaderboards: ranksWithName.slice(0,5)
+    })
 })
 
 // Logout route
 router.get('/logout', (req, res) => {
+    console.log("calling logout")
     req.session.destroy();
     res.redirect('/admin/login');
 });
@@ -27,5 +39,18 @@ router.get('/user-data', async(req, res) => {
     const userData = await UserController.userData()
     res.status(200).json(userData)
 })
+
+// router.get('/leader-boards', async(req, res) => {
+//     const leaderBoards = await leaderBoardService.getTopPredictionUserScore();
+//     let ranksWithName = leaderBoards.inter.map((user, index)=> {
+//         console.log(index===10477?user['5751']:"")
+//         return {
+//             name: user['5751']===''?'Unknown':user['5751'],
+//             phone: user['5752'],
+//             scores: user['5755']
+//         }
+//     })
+//     res.json(ranksWithName.sli)
+// })
 
 module.exports = router;
